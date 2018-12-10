87bf6525baa5a0559ce9dc4fa343df71ff7fc2be
Gerard Eklu <gerard_eklu@homedepot.com>
2018-12-10 14:51:44 -0500

    added describe function to put all test in and give description of the type of function we are testing

--- script.test.js
+++ script.test.js
@@ -1,12 +1,31 @@
 const googleSearch = require("./script");
 
-const dbMock = ["dog.com", "cheesepuff.com", "disney.com", "dogpictures.com"];
+const dbMock = [
+  "dog.com", 
+  "cheesepuff.com", 
+  "disney.com", 
+  "dogpictures.com"
+];
 
-it("This is a test", () => {
-  expect("hello").toBe("hello");
-});
 
-it("is searching google", () => {
-  expect(googleSearch("testtest", dbMock)).toEqual([]);
-  expect(googleSearch("dog", dbMock)).toEqual(["dog.com", "dogpictures.com"]);
-});
+describe('googleSearch', () => {
+  it("This is a test", () => {
+    expect("hello").toBe("hello");
+  });
+  
+  it("is searching google", () => {
+    expect(googleSearch("testtest", dbMock)).toEqual([]);
+    expect(googleSearch("dog", dbMock)).toEqual(["dog.com", "dogpictures.com"]);
+  });
+  
+  it('work with undefined and null input', () => {
+    expect(googleSearch(undefined, dbMock)).toEqual([]);
+    expect(googleSearch(null, dbMock)).toEqual([]);
+  })
+  
+  
+  it('does not return more than three matches', () => {
+    expect(googleSearch('.com', dbMock).length).toEqual(3)
+  })
+})
+
